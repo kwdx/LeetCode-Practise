344e633e5cc5807a6f7b8deb1c5b4b7b191d5f52
warden <wenduo_mail@163.com>
2022-08-04 22:34:51 +0800

    每日一题：1403

---
+++ Swift/1403.非递增顺序的最小子序列.swift
@@ -0,0 +1,55 @@
+/*
+ * @lc app=leetcode.cn id=1403 lang=swift
+ *
+ * [1403] 非递增顺序的最小子序列
+ */
+
+// @lc code=start
+class Solution {
+    func minSubsequence(_ nums: [Int]) -> [Int] {
+        var ans = [Int]()
+        let sum = nums.reduce(0, +)
+        let nums = nums.sorted()
+        var tmp = 0
+        for i in (0..<nums.count).reversed() {
+            tmp += nums[i]
+            ans.append(nums[i])
+            if tmp > sum / 2 {
+                break
+            }
+        }
+        return ans
+    }
+}
+// @lc code=end
+
+func main() {
+    var nums: [Int]
+    var res: [Int]
+    
+    /**
+     输入：nums = [4,3,10,9,8]
+     输出：[10,9]
+     解释：子序列 [10,9] 和 [10,8] 是最小的、满足元素之和大于其他各元素之和的子序列。但是 [10,9] 的元素之和最大。
+     */
+    nums = [4,3,10,9,8]
+    res = [10,9]
+    assert(res == Solution().minSubsequence(nums))
+    
+    /**
+     输入：nums = [4,4,7,6,7]
+     输出：[7,7,6]
+     解释：子序列 [7,7] 的和为 14 ，不严格大于剩下的其他元素之和（14 = 4 + 4 + 6）。因此，[7,6,7] 是满足题意的最小子序列。注意，元素按非递增顺序返回。
+     */
+    nums = [4,4,7,6,7]
+    res = [7,7,6]
+    assert(res == Solution().minSubsequence(nums))
+    
+    /**
+     输入：nums = [6]
+     输出：[6]
+     */
+    nums = [6]
+    res = [6]
+    assert(res == Solution().minSubsequence(nums))
+}
